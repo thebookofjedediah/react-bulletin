3402ab8a6a402145a201b2cb71a197df78643956
Harshit Pant <pantharshit00@gmail.com>
2019-05-18 12:08:56 +0530

    fix: rename graphql types  as to fix compatibility wiith wp graphql

--- src/graphql/apolloClient.js
+++ src/graphql/apolloClient.js
@@ -3,8 +3,8 @@ import { HttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 
 const client = new ApolloClient({
-  link: new HttpLink({ uri: 'http://bulletinlocaltest.local/graphql' }),
-  // link: new HttpLink({ uri: 'https://learn.wpgraphql.com/graphql' }),
+  // link: new HttpLink({ uri: 'http://bulletinlocaltest.local/graphql' }),
+  link: new HttpLink({ uri: 'https://www.wpgraphql.com/graphql' }),
   cache: new InMemoryCache()
 })
 
