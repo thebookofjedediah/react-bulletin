68a4fd3609bd6b461c747d9bc5a2bcdf173016b6
Jedediah Arnold <jarnold910@gmail.com>
2019-06-10 10:47:22 -0400

    stuck on styling the appbar and drawer - work on keeping drawer open and using rest of page

--- src/components/Layout/drawer.js
+++ src/components/Layout/drawer.js
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { getAllCategories } from '../../graphql/queries/categories'
 import Drawer from 'material-ui/Drawer'
+import Hidden from 'material-ui/Hidden'
 import Divider from 'material-ui/Divider'
 import IconButton from 'material-ui/IconButton'
 import ChevronLeftIcon from 'material-ui-icons/ChevronLeft'
@@ -11,79 +12,121 @@ import { Link } from 'react-router-dom'
 import AlarmClock from 'material-ui-icons/Alarm'
 import { graphql } from 'react-apollo'
 
-const SideComponent = ({
-  classes,
-  open,
-  handleDrawerClose,
-  data,
-  viewType
-}) => {
+const displayCategories = props => {
+  const { data, classes } = props
+  if (data.loading) return
+
   return (
     <div>
-      <Drawer
-        type='persistent'
-        classes={{
-          paper: classes.drawerPaper
-        }}
-        open={open}
-      >
-        <div className={classes.drawerInner}>
-          <div className={classes.drawerHeader}>
-            <Link to='/'>
-              <ListItem>
-                <img
-                  className={classes.image}
-                  alt='logo'
-                  src='https://franciscan.university/img/side-nav-logo.jpg'
-                />
-              </ListItem>
-            </Link>
-            <IconButton onClick={handleDrawerClose}>
-              <ChevronLeftIcon />
-            </IconButton>
-          </div>
-          <Divider />
-          <ListSubheader>Current Bulletin</ListSubheader>
-          <Link to='/' className={classes.link}>
-            <ListItem button>
-              <ListItemIcon>
-                <AlarmClock />
-              </ListItemIcon>
-              <ListItemText inset secondary='Take Action' />
-            </ListItem>
-          </Link>
-          <Divider />
+      <Link to={`/category/`} className={classes.link}>
+        <ListItem button>
+          <ListItemText secondary={`All`} />
+        </ListItem>
+      </Link>
+      {data.categories.edges.map(category => (
+        <Link
+          key={category.node.id}
+          to={`/category/${category.node.slug}`}
+          className={classes.link}
+        >
           <ListItem button>
-            <ListItemText primary='Categories' />
+            <ListItemText secondary={category.node.name} />
           </ListItem>
-          {!data.loading &&
-            data.categories &&
-            data.categories.edges.map(category => (
-              <Link
-                key={category.node.id}
-                to={`/category/${category.node.slug}`}
-                className={classes.link}
-              >
-                <ListItem button>
-                  <ListItemText secondary={category.node.name} />
-                </ListItem>
-              </Link>
-            ))}
-          <Divider />
-          <ListItem button>
-            <ListItemIcon>
-              <ClockIcon />
-            </ListItemIcon>
-            <ListItemText inset primary='Filter By Date' />
-          </ListItem>
-          <Divider />
-          <ListSubheader>Submit Announcement</ListSubheader>
-          <ListSubheader>Classic Mode</ListSubheader>
-          <ListSubheader>Help</ListSubheader>
-        </div>
-      </Drawer>
+        </Link>
+      ))}
     </div>
   )
+}
+
+class SideComponent extends Component {
+  render () {
+    const classes = this.props.classes
+    const drawer = (
+      <div className={classes.drawerInner}>
+        <div className={classes.drawerHeader}>
+          <ListItem>
+            <Link to='/'>
+              <img
+                className={classes.image}
+                alt='logo'
+                src='https://franciscan.university/img/side-nav-logo.jpg'
+              />
+            </Link>
+          </ListItem>
+          <IconButton
+            className={classes.navIconHide}
+            onClick={this.props.handleDrawerClose}
+          >
+            <ChevronLeftIcon />
+          </IconButton>
+        </div>
+        <Divider />
+        <Link to='/' className={classes.link}>
+          <ListItem button>
+            <ListItemText secondary='Current Bulletin' />
+          </ListItem>
+        </Link>
+        <Divider />
+        <Link to='/' className={classes.link}>
+          <ListItem button>
+            <ListItemIcon>
+              <AlarmClock />
+            </ListItemIcon>
+            <ListItemText inset secondary='Take Action' />
+          </ListItem>
+        </Link>
+        <Divider />
+        <ListSubheader>Categories</ListSubheader>
+        {displayCategories(this.props)}
+        <Divider />
+        <ListItem button>
+          <ListItemIcon>
+            <ClockIcon />
+          </ListItemIcon>
+          <ListItemText inset primary='Filter By Date' />
+        </ListItem>
+        <Divider />
+        <ListSubheader
+          className={classes.btn}
+          onClick={this.props.toggleDrawer}
+        >
+          Submit Announcement
+        </ListSubheader>
+        <ListSubheader>Classic Mode</ListSubheader>
+        <ListSubheader>Help</ListSubheader>
+      </div>
+    )
+    return (
+      <div>
+        <Hidden mdUp>
+          <Drawer
+            variant='temporary'
+            open={this.props.open}
+            classes={{
+              paper: classes.drawerPaper
+            }}
+            onRequestClose={this.props.handleDrawerToggle}
+            ModalProps={{
+              keepMounted: true // Better open performance on mobile.
+            }}
+          >
+            {drawer}
+          </Drawer>
+        </Hidden>
+        <Hidden mdDown implementation='css'>
+          <Drawer
+            type='permanent'
+            open
+            classes={{
+              paper: classes.drawerPaper
+            }}
+          >
+            {drawer}
+          </Drawer>
+        </Hidden>
+      </div>
+    )
+  }
 }
 
 export default graphql(getAllCategories)(SideComponent)
