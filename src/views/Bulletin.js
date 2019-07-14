cd3f87f7b7bfc6b2f148142a992637d70bbe034c
Harshit Pant <pantharshit00@gmail.com>
2019-07-14 17:41:50 +0530

    fix: infinite and implement bulletin

--- src/views/Bulletin.js
+++ src/views/Bulletin.js
@@ -1,14 +1,13 @@
 import React from 'react'
-import { graphql } from 'react-apollo'
 import { getAllPosts } from '../graphql/queries/posts'
 import dayjs from 'dayjs'
 import Layout from '../components/Layout/index'
-import Loader from '../components/Loader'
 import GridRenderer from '../components/GridTypes/GridRenderer'
 import { Helmet } from 'react-helmet'
-import Error from '../components/Error'
 
-const Home = ({ data, viewtype, searchposts }) => {
+const week = dayjs().startOf('week')
+
+const Bulletin = ({ data, viewtype, searchposts }) => {
   return (
     <Layout>
       <RenderHome data={data} viewtype={viewtype} searchposts={searchposts} />
@@ -18,35 +17,28 @@ const Home = ({ data, viewtype, searchposts }) => {
 }
 
 const RenderHome = ({ data, viewtype, searchposts }) => {
-  const posts = searchposts || data.posts
   return (
     <>
       <Helmet>
         <title>Home | Bulletin - Franciscan University of Steubenville</title>
       </Helmet>
-      {!data.error && !posts && <Loader />}
-      {data.error && <Error error={data.error.message} />}
-      {posts && (
-        <GridRenderer posts={posts} viewtype={viewtype} query={getAllPosts} />
-      )}
+      <GridRenderer
+        viewtype={viewtype}
+        query={getAllPosts}
+        variables={{
+          where: {
+            dateQuery: {
+              after: {
+                day: week.date(),
+                month: week.month() + 1,
+                year: week.year()
+              }
+            }
+          }
+        }}
+      />
     </>
   )
 }
 
-const week = dayjs().startOf('week').day
-
-export default graphql(getAllPosts, {
-  options: {
-    variables: {
-      where: {
-        dateQuery: {
-          after: {
-            day: week.day,
-            month: week.month,
-            year: week.year
-          }
-        }
-      }
-    }
-  }
-})(Home)
+export default Bulletin
